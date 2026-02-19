8ee4193cde8b65701226b0b1c156a1375b5e4180
kokoro2580 <hatsakon5806@gmail.com>
2026-02-19 23:20:24 +0700

    Delete AreaCalculator.java

--- AreaCalculator.java
+++
@@ -1,7 +0,0 @@
-package oop.ocp;
-
-public class AreaCalculator {
-    public void calculateArea(Shape shape){
-        System.out.println(shape.calculateArea());///คำนวนรูปร่างโดยไม่ต้องแก้โค้ด
-    }
-}
